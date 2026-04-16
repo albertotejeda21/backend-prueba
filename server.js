f0926df7a959520f716167bd232d871256847739
alberto <albertotejeda2081@gmail.com>
2026-04-16 17:37:32 -0600

    despliegue

---
+++ server.js
@@ -0,0 +1,74 @@
+
+//aqui se define la bd
+
+const express = require("express");
+const mysql = require("mysql2");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+//ngrok
+app.use((req, res, next) => {
+    res.setHeader('ngrok-skip-browser-warning', 'true');
+    next();
+});
+
+
+// conexión a MariaDB
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "wereables"
+});
+
+
+
+
+// verificar conexión
+db.connect(err => {
+    if (err) {
+        console.log("Error conexión BD:", err);
+    } else {
+        console.log("Conectado a MariaDB");
+    }
+});
+
+// INSERTAR datos
+app.post("/health", (req, res) => {
+    const { steps, heartRate, distance, active_calories, total_calories } = req.body;
+
+    const query = "INSERT INTO health_data (steps, heart_rate, distance, active_calories, total_calories) VALUES (?, ?, ?, ?, ?)";
+
+    db.query(query, [steps, heartRate, distance, active_calories, total_calories], (err) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json(err);
+        }
+        res.json({ message: "Datos guardados" });
+    });
+});
+
+
+
+// OBTENER datos
+app.get("/health", (req, res) => {
+    db.query("SELECT * FROM health_data ORDER BY id DESC", (err, result) => {
+        if (err) return res.status(500).json(err);
+        res.json(result);
+    });
+});
+
+app.get("/", (req, res) => {
+    res.send("✅ Servidor Node funcionando y conectado al túnel!");
+});
+
+
+
+app.listen(3000, "0.0.0.0", () => {
+    console.log("API corriendo en http://localhost:3000");
+});
